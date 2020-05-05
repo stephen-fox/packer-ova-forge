39bc523c77ab881bb53b5fe8ff09ebfd81e35a4b
Stephen Fox <stephen.j.fox.jr@gmail.com>
2020-05-05 11:54:38 -0400

    Use basic bash scripts for build process.

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ -z "${VERSION}" ]]
+then
+    echo 'the VERSION environment variable must be set'
+    exit 1
+fi
+
+set -eux
+
+buildDir='build'
+mkdir -p "${buildDir}"
+
+filename='packer-post-processor-ova-forge'
+if [[ ! -z "${GOOS+x}" ]]
+then
+    filename="${filename}-${GOOS}"
+fi
+if [[ ! -z "${GOARCH+x}" ]]
+then
+    filename="${filename}-${GOARCH}"
+fi
+if [[ ! -z "${GOOS+x}" ]] && [[ "${GOOS}" == "windows" ]]
+then
+    filename="${filename}.exe"
+fi
+
+go build -ldflags "-X main.version=${VERSION}" -o "${buildDir}/${filename}" cmd/packer-*/main.go
